10244dc16feb18b98c85d835f1202a20dd0b5099
Andrey Bezpalov <abezpalov@ya.ru>
2020-09-26 11:39:04 +0400

    Axoft

---
+++ anodos/swarm/workers/axoft_events_monitoring.py
@@ -0,0 +1,54 @@
+from swarm.models import *
+from swarm.workers.worker import Worker
+
+
+class Worker(Worker):
+
+    name = 'axoft.ru/news'
+    login = None
+    password = None
+    start_url = 'https://axoft.ru/current/events/list/'
+    base_url = 'https://axoft.ru'
+    company = 'Axoft'
+
+    def __init__(self):
+        self.source = Source.objects.take(
+            name=self.name,
+            login=self.login,
+            password=self.password)
+        super().__init__()
+
+    def run(self):
+
+        # Заходим на первую страницу
+        tree = self.load(url=self.start_url, result_type='html')
+
+        # Получаем все ссылки
+        items = tree.xpath('//div[@class="b-content_block"]/div')
+        items.reverse()
+
+        for item in items:
+            news_type = 'Новость'
+            try:
+                title = item.xpath('.//h3//a/text()')[0]
+                url = item.xpath('.//h3//a/@href')[0]
+                term = item.xpath('.//*[@class="b-date_big"]/text()')[0]
+                text = item.xpath('.//p[@class="b-content_text"]/text()')[0]
+            except IndexError:
+                continue
+            if not url.startswith(self.base_url):
+                url = '{}{}'.format(self.base_url, url)
+
+            title = self.fix_text(title)
+            term = self.fix_text(term)
+            text = self.fix_text(text)
+
+            try:
+                SourceData.objects.get(source=self.source, url=url)
+            except SourceData.DoesNotExist:
+                content = f'<b>{news_type} {self.company}</b>\n<i>{term}</i>\n\n<a href="{url}">{title}</a>\n{text}\n'
+                print(content)
+                self.send(content)
+                data = SourceData.objects.take(source=self.source, url=url)
+                data.content = content
+                data.save()
---
+++ swarm_monitoring.sh
@@ -0,0 +1,16 @@
+timeout 10s ping ya.ru
+
+timeout 2m ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py ocs_promo_monitoring
+timeout 2m ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py ocs_events_monitoring
+timeout 2m ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py ocs_news_monitoring
+
+timeout 2m ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py axoft_events_monitoring
+timeout 2m ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py axoft_news_monitoring
+
+timeout 2m ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py mont_events_monitoring
+timeout 2m ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py mont_news_monitoring
+
+timeout 2m ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py fujitsu_news_monitoring
+timeout 2m ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py fujitsu_blog_monitoring
+
+timeout 2m ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py maed_webinars_monitoring
