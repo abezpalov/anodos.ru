d283e1f03c61eb779c0a13f1ae586eb5f670d9ed
Andrey Bezpalov <abezpalov@ya.ru>
2021-09-28 00:55:15 +0400

    Treolan on prod

--- everyday_update.sh
+++ everyday_update.sh
@@ -1,5 +1,6 @@
 timeout 10s ping ya.ru
 
 timeout 6h ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py ocs update_stocks
+timeout 6h ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py treolan update_stocks
 
 timeout 12h ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py ocs update_content_changes
