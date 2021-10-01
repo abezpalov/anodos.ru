05082f3a478316bfde75f3b7183d0521b2c15e16
Andrey Bezpalov <abezpalov@ya.ru>
2021-10-02 01:00:15 +0400

    Скорректировал расписание

--- everyday_update.sh
+++ everyday_update.sh
@@ -4,3 +4,4 @@ timeout 6h ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py ocs upda
 timeout 6h ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py treolan update_stocks
 
 timeout 12h ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py ocs update_content_changes
+timeout 48h ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py treolan update_content_clear
--- everymonth_update.sh
+++ everymonth_update.sh
@@ -1,3 +1,4 @@
 timeout 10s ping ya.ru
 
-timeout 24h ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py ocs update_content_changes_all
+timeout 48h ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py ocs update_content_all
+timeout 48h ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py treolan update_content_all
