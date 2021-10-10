680a32fe1dbff3844641840f6d5ace5af896e4c3
Andrey Bezpalov <abezpalov@ya.ru>
2021-10-11 02:04:23 +0400

    Оптимизация роботов

--- everyday_update.sh
+++ everyday_update.sh
@@ -2,7 +2,8 @@ timeout 10s ping ya.ru
 
 timeout 6h ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py ocs update_stocks
 timeout 6h ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py treolan update_stocks
-timeout 48h ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py service
 
 timeout 12h ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py ocs update_content_changes
 timeout 48h ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py treolan update_content_clear
+
+timeout 48h ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py service
