d8bd540f5fa14218c4ee0f058ec0425c27f816e9
Andrey Bezpalov <abezpalov@ya.ru>
2021-10-12 21:12:33 +0400

    Fix

--- everymonth_update.sh
+++ everymonth_update.sh
@@ -1,4 +1,4 @@
 timeout 10s ping ya.ru
 
-timeout 48h ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py ocs update_content_all
-timeout 48h ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py treolan update_content_all
+timeout 128h ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py ocs update_content_all
+timeout 128h ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py treolan update_content_all
