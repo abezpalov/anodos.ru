0cab75ac8d04ca2cb7f2b703ae3912ce24d45737
Andrey Bezpalov <abezpalov@ya.ru>
2021-11-16 00:00:57 +0400

    fix

--- everyweek.sh
+++ everyweek.sh
@@ -1,5 +1,5 @@
 ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py ocs update_content_all
 ~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py treolan update_content_all
 
-~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py service fix_products
-~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py service fix_parameter_values
+~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py service rewrite_products
+~/anodos.ru/venv/bin/python3 ~/anodos.ru/anodos/swarm/run.py service rewrite_parameter_values
